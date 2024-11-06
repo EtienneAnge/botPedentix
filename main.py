38f9bc622490f5ba6dc516321f3ac13f31660844
Étienne Angé <etienne.a2004@gmail.com>
2024-11-06 07:39:54 +0100

    push

--- main.py
+++ main.py
@@ -25,67 +25,76 @@ driver.find_element(By.XPATH, "/html/body/div[2]/div[2]/div[1]/div[1]/button/spa
 driver.find_element(By.XPATH, "//*[@id='dialog-close']").click()
 
 input_case = driver.find_element(By.XPATH, "//*[@id='pedantix-guess']")
+enter_case = driver.find_element(By.XPATH, "//*[@id='pedantix-guess-btn']")
 
+ 
+# tab contenent la discussion avec chatgpt
+histoMess=[{
+            "role": "user",
+            "content": "je joue au pédentix : je dois trouvé une page wikipedia (caché) avec des mots.Tu dois me donner 50 mots, sachant que tu ne dois jamais me donner un mot que tu m'as déjà donné et tu ne dois pas me donner de mot facile du genre déterminant, etc. J'utilise tas réponse dans un script python donc tu dois seulement me répondre par les mot et les mots doivent être séparé par '-'",
+        }]
 
-#input_case.send_keys("un", Keys.ENTER)
-
-tab = []
-
-start_time = time.time()
-
-id = 0
+#historique du tableau des mots pour ne pas remettre les meme mot a chaque fois mais uniquement les nouveaux
+tabHisto = []
 
 while True:
 
-    try:
-        # Tente de trouver l'élément par son id
-        node = driver.find_element(By.ID, id)
-    except:
-        # Si l'élément n'est pas trouvé
-        break
+    # I. cherche les mot de la page et les stocke dans un tableau
 
-    if(node.text.strip() != ""):
-        tab.append(node.text)
-        #print(node.text)
+    #tableau contenent les mots de la page
+    tab = []
+
+    #cherche les mots avec les id
+    id = 0
+
+    while True:
+
+        try:
+            # Tente de trouver l'élément par son id
+            node = driver.find_element(By.ID, id)
+        except:
+            # Si l'élément n'est pas trouvé
+            break
+
+        #enleve les mots non trouvé
+        if(node.text.strip() != ""):
+            tab.append(node.text)
+            #print(node.text)
+        
+        id = id + 1
+        
+
+    #print(tab)
+
+    tab = [mott for mott in tab if mott not in tabHisto]
+    for mot in tab:
+        tabHisto.append(mot)
     
-    id = id + 1
-    
+    print(tab)
+    if tab == []:
+        histoMess.append({"role": "user","content": "continue avec la consigne du début. Aucun nouveau mot trouvé"})
+    else:
+        histoMess.append({"role": "user","content": ("continue avec la consigne du début. Voici les nouveaux mots trouvés " + str(tab))})
 
-print(tab)
-
-
-end_time = time.time()
-print(end_time - start_time)
-
-
-histoMess=[{
-        "role": "user",
-        "content": "je joue au pédentix.Tu dois me donner 10 mot. Tu dois seulement me répondre par les mot et les mots doivent être séparé par '-'",
-    },
-    {
-        "role": "assistant",
-        "content": "d'accord alors allons y",
-    }]
-
-#histoMess.append({"role": "user","content": ("voici les mots trouvé " + str(tab))})
-
-response = client.chat.completions.create(
-    messages = histoMess,
-    model="gpt-3.5-turbo",
-)
+    response = client.chat.completions.create(
+        messages = histoMess,
+        model="gpt-3.5-turbo",
+    )
 
 
-# Affiche la réponse
-print(response.choices[0].message.content)
 
-histoMess.append({"role": "assistant","content": response.choices[0].message.content})
+    histoMess.append({"role": "assistant","content": response.choices[0].message.content})
 
-tableau = response.choices[0].message.content.split('-')
+    tableau = response.choices[0].message.content.split('-')
 
-for i in range(0,10):
-    input_case.send_keys(tableau[i], Keys.ENTER)
-    time.sleep(0.05)
+    for mot in tableau:
+        input_case.send_keys(mot)
+        enter_case.click()
+
 print(histoMess)
+
+
+
 #driver.close()
 
 
---
+++ test.py
@@ -0,0 +1,7 @@
+
+tabHisto = ["mot1", "mot2"]
+tab = ["mot3", "mot1"]
+
+
+tab = [mott for mott in tab if mott not in tabHisto]
+print(tab)
